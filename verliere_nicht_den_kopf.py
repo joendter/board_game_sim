ce98cffb316c287a2128d92d071f80af3fa194bf
joendter <71767955+joendter@users.noreply.github.com>
2022-12-25 18:53:25 +0100

    redid some validity checks
    should now accurately represent the actual game

--- verliere_nicht_den_kopf.py
+++ verliere_nicht_den_kopf.py
@@ -1,4 +1,5 @@
 from random import randint
+import random
 from copy import deepcopy
 from time import time
 
@@ -13,32 +14,32 @@ def rep(x,n):
 sidelength = 10
 playernumber = 4
 playerpieces = 4
-#pos 0 means start
-#neg position means target; -1 first one
+#pos -1 means start
+#neg position means target; -2 first one
 
 class player:
     def __init__(self, pieces, home, ai):
-        self.pieces = [0]*pieces
+        self.pieces = [-1]*pieces
         self.home = home
         self.piecenumber = pieces
         self.ai = ai
 
     def won(self):
-        return max(self.pieces) < 0
+        return max(self.pieces) < -1
 
     def three_rolls(self):
-        if max(self.pieces) > 0:
+        if max(self.pieces) >= 0:
             return False
 
         #abspieces = [abs(p) for p in self.pieces]
         housepieces = []
         for p in self.pieces:
-            if p != 0:
+            if p < -1:
                 housepieces.append(p)
         if len(housepieces) == 0:
             return True
 
-        if min(housepieces) == - self.piecenumber and max(housepieces) == - self.piecenumber + len(housepieces) - 1:
+        if min(housepieces) == - self.piecenumber - 1 and max(housepieces) == - self.piecenumber + len(housepieces) - 2:
             return True
 
         return False
@@ -46,7 +47,7 @@ class player:
 
 
 class board:
-    def __init__(self, sidelength, playernumber, playerpieces, *ais):
+    def __init__(self, sidelength, playernumber, playerpieces, *ais, debug = False):
         self.sidelength = sidelength
         self.playernumber = playernumber
         self.playerpieces = playerpieces
@@ -54,9 +55,10 @@ class board:
         self.players = []
         self.playertargets = []
         self.winner = -1
+        self.debug = debug
         #print(ais)
         for playerID in range(playernumber):
-            self.players.append(player(playerpieces,playerID*sidelength +1, ais[playerID]))
+            self.players.append(player(playerpieces,playerID*sidelength, ais[playerID]))
 
     def play(self):
         while self.winner == -1:
@@ -76,26 +78,41 @@ class board:
 
     def playerturn(self, playerID):
         dicethrow = randint(1,6)
-        playerAI = self.players[playerID].ai
-        self.move(playerID,playerAI(deepcopy(self),dicethrow,playerID),dicethrow)
+        move = self.players[playerID].ai(deepcopy(self),dicethrow,playerID)
+        self.move(playerID,move,dicethrow)
         if self.players[playerID].won():
             print(f"Player {playerID} won")
             self.winner = playerID
+        if self.debug:
+            print(f"Player {playerID} moved piece {move} with diceroll {dicethrow}")
+            print(f"Current positions of his pieces: {self.players[playerID].pieces}")
         if dicethrow == 6:
             return True
         return False
 
     def kill(self, playerID, square):
+        if square < 0:
+            return 0
         for player in range(self.playernumber):
             if player != playerID:
                 for i in range(self.players[player].piecenumber):
                     if square == self.players[player].pieces[i]:
-                        self.players[player].pieces[i] = 0
+                        self.players[player].pieces[i] = -1
+        return 1
 
-
+    def kill_try(self, playerID, square):
+        if square < 0:
+            return 0
+        for player in range(self.playernumber):
+            if player != playerID:
+                for i in range(self.players[player].piecenumber):
+                    if square == self.players[player].pieces[i]:
+                        return 1
+        return 0
 
     def move(self, playerID, pieceID, dicethrow):
         player = self.players[playerID]
+        comeout = False
 
         if player.won():
             return 1
@@ -104,21 +121,29 @@ class board:
         house = position < 0
 
         if not house:
-            new_position = position + dicethrow % self.size
+            new_position = (position + dicethrow) % self.size
         else:
             new_position = position - dicethrow
 
-        if position == 0 and dicethrow != 6:
+        if position == -1 and dicethrow != 6:
             return 0
 
-        if position == 0 and dicethrow == 6:
+        if position == -1 and dicethrow == 6:
             new_position = player.home
+            comeout = True
+
+        if dicethrow == 6 and player.home not in player.pieces and position != -1:
+            return 0
 
         if new_position in player.pieces:
             return 0
 
-        if new_position >= player.home > position or position > new_position >= player.home:
-            new_position = player.home - new_position - 1
+        if player.home in player.pieces and position != player.home and not player.home + dicethrow in player.pieces:
+            return 0
+
+        if (position + dicethrow >= player.home > position or position + dicethrow >= player.home + self.size > position)\
+                and not comeout:
+            new_position = player.home - new_position - 2
             house = True
 
         if house:
@@ -126,7 +151,7 @@ class board:
                 if piece != pieceID and position > player.pieces[piece] >= new_position:
                     return 0
 
-            if new_position < -player.piecenumber:
+            if new_position < -player.piecenumber - 1:
                 return 0
 
         self.kill(playerID,new_position)
@@ -135,6 +160,7 @@ class board:
 
     def validmove(self, playerID, pieceID, dicethrow):
         player = self.players[playerID]
+        comeout = False
 
         if player.won():
             return 1
@@ -143,21 +169,29 @@ class board:
         house = position < 0
 
         if not house:
-            new_position = position + dicethrow % self.size
+            new_position = (position + dicethrow) % self.size
         else:
             new_position = position - dicethrow
 
-        if position == 0 and dicethrow != 6:
+        if position == -1 and dicethrow != 6:
             return 0
 
-        if position == 0 and dicethrow == 6:
+        if position == -1 and dicethrow == 6:
             new_position = player.home
+            comeout = True
+
+        if dicethrow == 6 and player.home not in player.pieces and position != -1:
+            return 0
 
         if new_position in player.pieces:
             return 0
 
-        if new_position >= player.home > position or position > new_position >= player.home:
-            new_position = player.home - new_position - 1
+        if player.home in player.pieces and position != player.home and not player.home + dicethrow in player.pieces:
+            return 0
+
+        if (position + dicethrow >= player.home > position or position + dicethrow >= player.home + self.size > position)\
+                and not comeout:
+            new_position = player.home - new_position - 2
             house = True
 
         if house:
@@ -165,10 +199,17 @@ class board:
                 if piece != pieceID and position > player.pieces[piece] >= new_position:
                     return 0
 
-            if new_position < -player.piecenumber:
+            if new_position < -player.piecenumber - 1:
                 return 0
 
         return 1
+
+    def valid(self, dicethrow: int, playerID: int):
+        v = []
+        for i in range(self.players[playerID].piecenumber):
+            if self.validmove(playerID, i, dicethrow):
+                v.append(i)
+        return v
 
 def aiRandom(board: board, dicethrow: int, playerID: int):
     return randint(0,board.players[playerID].piecenumber-1)
@@ -177,17 +218,14 @@ def aiZero(board: board, dicethrow: int, playerID: int):
     return 0
 
 def aiValidMin(board: board, dicethrow: int, playerID: int):
-    for i in range(board.players[playerID].piecenumber):
-        if board.validmove(playerID,i,dicethrow):
-            return int(i)
+    valid = board.valid(dicethrow,playerID)
+    if len(valid) > 0:
+        return valid[0]
     return 0
 
 
 def aiValidRandom(board: board, dicethrow: int, playerID: int):
-    valid = []
-    for i in range(board.players[playerID].piecenumber):
-        if board.validmove(playerID,i,dicethrow):
-            valid.append(i)
+    valid = board.valid(dicethrow, playerID)
     if len(valid) > 0:
         return valid[randint(0,len(valid)-1)]
     return 0
@@ -197,9 +235,13 @@ def aiValidRandom(board: board, dicethrow: int, playerID: int):
 
 results = [0]*4
 
+random.seed(42)
+
+a = board(sidelength,playernumber,playerpieces,aiValidMin,aiValidMin,aiValidMin,aiValidMin, debug=True)
+a.play()
 
 t0 = time()
-for i in range(7000):
+for i in range(128):
     if i % 128 == 0:
         print(i)
         print(results)
